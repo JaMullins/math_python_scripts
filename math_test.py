15756c5e50d5b6ada7e888b8984f34716c8042fb
scamRlikely <114003827+scamRlikely@users.noreply.github.com>
2024-01-11 00:12:34 -0500

    Update math_test.py

--- math_test.py
+++ math_test.py
@@ -10,6 +10,24 @@ def geoSequence():
     print(num*frac)
 #geoSequence()
 
+def arithSequence():
+    num = float(input("enter first term : ")) #just the first term of equation
+    sumNum = float(input("enter sumNum : ")) #Amount of terms you want to be added together, hence "sumNum"
+    test = int(input("enter arithmatic num : ")) #Just incase it's arithmetic
+    list = []
+    f = test*sumNum
+    fin = num + f
+    for x in range(test):
+        g = x*test
+        h = num + g
+        list.append(h)
+        if x != test:
+            break
+    print(fin)
+    print(sum(list))
+arithSequence()
+
+
 def findZero():
     start = float(input("enter first term : "))
     ratio = float(input("enter common ratio : "))
